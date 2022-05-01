cb541e8202405b66350f2b1320180455e375f3f6
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-05-01 13:53:26 +0800

    menu

--- src/main/main.js
+++ src/main/main.js
@@ -1,3 +1,6 @@
 import './reset.scss';
 import './main.scss';
 console.log('Hello main.js!');
+document.querySelector('#menu__toggle').addEventListener('change', function (e) {
+  document.body.style.overflow = e.target.checked === true ? 'hidden' : '';
+});
